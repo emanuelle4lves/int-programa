c9dd573641f52144389d7ae2168e397190af869d
Emanuelle Alves <122374698+emanuelle4lves@users.noreply.github.com>
2025-09-17 10:12:23 -0300

    Questao 2

--- questao2.py
+++ questao2.py
@@ -2,4 +2,9 @@ nome = input("Qual seu nome? ")
 
 print (f"Olá, {nome}")
 
-numero = int(input(" Digite um número; "))
+n1 = int(input(" Digite um número; "))
+n2 = int(input(" Digite outro número; "))
+
+resultado =  n1+n2
+
+print(f"A somas desses numeros é; {resultado}")
